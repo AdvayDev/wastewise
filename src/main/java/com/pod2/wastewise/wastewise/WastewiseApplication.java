d9a101a29c09a3e026e09b4df300310b77d4008d
Banugariya <2408482@cognizant.com>
2025-05-26 14:20:02 +0530

    Initial WasteWise Project setup

---
+++ src/main/java/com/pod2/wastewise/wastewise/WastewiseApplication.java
@@ -0,0 +1,13 @@
+package com.pod2.wastewise.wastewise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WastewiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WastewiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pod2/wastewise/wastewise/WastewiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pod2.wastewise.wastewise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WastewiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
