440379f5fe099cca8fea4bdfeb8ab5aaf2d56f90
Advay Banugariya <advay.banugariya@cognizant.com>
2025-05-27 18:02:32 +0530

    Added a utility class 'IdGenerator' and then created workerService class in service layer

--- src/main/java/com/pod2/wastewise/repository/VehicleRepository.java
+++ src/main/java/com/pod2/wastewise/repository/VehicleRepository.java
@@ -1,10 +1,13 @@
 package com.pod2.wastewise.repository;
 
 import com.pod2.wastewise.model.Vehicle;
+import com.pod2.wastewise.model.enums.VehicleType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface VehicleRepository extends JpaRepository<Vehicle,String> {
     @Query("Select COUNT(v) from Vehicle v")
     long countAll();
+
+    long countByType(VehicleType type);
 }
--- src/main/java/com/pod2/wastewise/repository/WorkerRepository.java
+++ src/main/java/com/pod2/wastewise/repository/WorkerRepository.java
@@ -2,7 +2,9 @@ package com.pod2.wastewise.repository;
 
 import com.pod2.wastewise.model.Worker;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface WorkerRepository extends JpaRepository<Worker,String> {
-
+    @Query("Select COUNT(w) from Worker w")
+    long countAll();
 }
